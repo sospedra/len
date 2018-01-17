553fb4415eeb1d1e771e1b17cc807bc44c14a38c
sospedra <sospedra.r@gmail.com>
2018-01-17 16:16:42 +0100

    refactor: Remove deprecated lodash.isarray

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const defaultTo = require('lodash.defaultto')
 const get = require('lodash.get')
-const isArray = require('lodash.isarray')
 
 const DEFAULT_LENGTH = 0
 
 module.exports = function len (target) {
-  return isArray(target)
+  return Array.isArray(target)
     ? defaultTo(get(target, 'length'), DEFAULT_LENGTH)
     : DEFAULT_LENGTH
 }
